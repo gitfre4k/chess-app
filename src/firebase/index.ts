f98695a51a12b1b52b4d1d1d777220d2f343ea09
gitfre4k <zatocilo@gmail.com>
2022-03-03 21:55:47 +0100

    hide api keys :)

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,17 +3,17 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDEgAJ00rCxBbwkjzk4McoWaLIfosv9DOk",
-  authDomain: "chess-app-34f1a.firebaseapp.com",
-  projectId: "chess-app-34f1a",
-  storageBucket: "chess-app-34f1a.appspot.com",
-  messagingSenderId: "210696268463",
-  appId: "1:210696268463:web:c06c71ff7e9b619a7b425a",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
-const db = getFirestore();
-const auth = getAuth();
+const db = getFirestore(app);
+const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
 export { db, auth, provider };
